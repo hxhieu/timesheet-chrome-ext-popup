64a97bf5dd6c12d558c8709082477abf3a0d4401
Hugh Hoang <hugh.hoang@gmail.com>
2021-05-20 19:45:57 +1200

    wip: day gauge

---
+++ src/features/weekly/DayGauge.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react';
+import styled from '@emotion/styled';
+
+const Wrapper = styled.div``;
+
+const DayGauge = (): JSX.Element => {
+  return <Wrapper />;
+};
+
+export default DayGauge;
